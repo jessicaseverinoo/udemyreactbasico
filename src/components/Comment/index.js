e239fe63b79f7f1839767f2fc9b6bb0cd4a15482
Jessica Severino <jessica.severino@accurate.com.br>
2020-12-27 10:34:20 -0300

    (feat): construindo components

---
+++ src/components/Comment/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './comment.css'
+
+const Comment = () => {
+  return (
+    <>
+      <div>
+        <div className='comment'>
+          <p>My first comment</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Comment
